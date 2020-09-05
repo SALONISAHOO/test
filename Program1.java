057324ac605ae4455749cc0fd394e19540aa7fa5
Saloni Sahoo <sahoosaloni15@gmail.com>
2020-09-05 17:07:44 +0530

    program file added .

---
+++ Program1.java
@@ -0,0 +1,20 @@
+public class Program1
+{
+	public static void main(String[] args)
+	{
+		int i,n;
+
+		char ch;
+
+		 
+		for(i=65;i<=90;i++)
+		{
+		 n=i+2;
+		  System.out.println((char)n);
+		 }
+		
+
+
+		
+	}
+}
